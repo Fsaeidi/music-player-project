5970664ba14ede9d342ceb05f18a7fea843955c3
Fsaeidi <129076603+Fsaeidi@users.noreply.github.com>
2023-04-20 17:40:40 +0330

    Add files via upload
    commit

---
+++ Song.js
@@ -0,0 +1,66 @@
+import { v4 as uuidv4 } from 'uuid';
+
+
+export default function Songs(){
+    return[
+    {
+    id:uuidv4(),
+    name:'عمو زنجیرباف',
+    artist:"Arash&Masih",
+    cover:"https://musico.ir/wp-content/uploads/2021/11/IMG_08112021_173115_450_x_450_pixel.jpg",
+    active:true,
+    audio:"https://files.musico.ir/siros/Masih%20And%20Arash%20-%20Amo%20Zanjrbaf%20(320).mp3"
+    },
+    {
+        id:uuidv4(),
+        name:'یه صبح دیگه یه صدایی توی گوشم میگه',
+        artist:"sirvan",
+        cover:"https://musico.ir/wp-content/uploads/2019/08/%DA%86%D9%88.jpg",
+        active:false,
+        audio:"https://files.musico.ir/siros/Masih%20And%20Arash%20-%20Amo%20Zanjrbaf%20(320).mp3"
+        },
+        {
+            id:uuidv4(),
+            name:'ماه عسل',
+            artist:"ased",
+            cover:"https://musico.ir/wp-content/uploads/2023/04/Hamed-Bardaran-Mah-Asal-Musico.ir_.jpg",
+            active:false,
+            audio:"https://files.musico.ir/siros/Hamed%20Bardaran%20-%20Mah%20Asal%20(320).mp3"
+            },
+            {
+                id:uuidv4(),
+                name:'رفتی',
+                artist:"hirad",
+                cover:"https://musico.ir/wp-content/uploads/2023/04/Hamid-Hiraad-Rafti-Musico.ir_.jpg",
+                active:false,
+                audio:"https://files.musico.ir/siros/Hamid%20Hirad%20-%20Rafti%20(320).mp3"
+                },
+                {
+                    id:uuidv4(),
+                    name:'رفتی',
+                    artist:"hirad",
+                    cover:"https://musico.ir/wp-content/uploads/2023/04/Hamid-Hiraad-Rafti-Musico.ir_.jpg",
+                    active:false,
+                    audio:"https://files.musico.ir/siros/Hamid%20Hirad%20-%20Rafti%20(320).mp3"
+                    },
+                    {
+                        id:uuidv4(),
+                        name:'رفتی',
+                        artist:"hirad",
+                        cover:"https://musico.ir/wp-content/uploads/2023/04/Hamid-Hiraad-Rafti-Musico.ir_.jpg",
+                        active:false,
+                        audio:"https://files.musico.ir/siros/Hamid%20Hirad%20-%20Rafti%20(320).mp3"
+                        },
+                        {
+                            id:uuidv4(),
+                            name:'رفتی',
+                            artist:"hirad",
+                            cover:"https://musico.ir/wp-content/uploads/2023/04/Hamid-Hiraad-Rafti-Musico.ir_.jpg",
+                            active:false,
+                            audio:"https://files.musico.ir/siros/Hamid%20Hirad%20-%20Rafti%20(320).mp3"
+                            }
+
+]
+}
+
+    
